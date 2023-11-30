fd03b21a615006d920ddbcf68dba1612f2fc8c5d
Javier Martínez Megías <jmarmeg1005@g.educaand.es>
2023-11-30 19:27:16 +0100

    CommandLine

--- Models/src/main/java/es/reaktor/models/Computer.java
+++ Models/src/main/java/es/reaktor/models/Computer.java
@@ -1,5 +1,12 @@
 package es.reaktor.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Computer
 {
 	/**
