2b50e69db8d839aa47bd365337a30f2d6d98bfca
DavidMartinezFlores <dmarflo526@g.educaand.es>
2023-11-30 19:07:48 +0100

    "CREATING-ADMINISTRATION-REST"

---
+++ Reaktor/src/main/java/es/reaktor/reaktor/rest/ReaktorAdministrationRest.java
@@ -0,0 +1,63 @@
+package es.reaktor.reaktor.rest;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author David Martinez
+ *
+ */
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/computers")
+public class ReaktorAdministrationRest
+{
+	@RequestMapping(method = RequestMethod.GET, value = "/admin/commandLine")
+	public ResponseEntity<?> sendInformation(
+			@RequestHeader(required = false) String serialNumber,
+			@RequestHeader(required = false) String classroom, 
+			@RequestHeader(required = false) String trolley,
+			@RequestHeader(required = false) Integer plant)
+	{
+		try
+		{
+			if (serialNumber != null || classroom != null || trolley != null || plant != null)
+			{
+				if(serialNumber!=null) 
+				{
+					return ResponseEntity.ok("CommandLine send OK serialNumber");
+				}
+				else if(trolley!=null) 
+				{
+					return ResponseEntity.ok("CommandLine send OK trolley");
+				}
+				else if(classroom!=null) 
+				{
+					return ResponseEntity.ok("CommandLine send OK classroom");
+				}
+				else if(plant!=null) 
+				{
+					return ResponseEntity.ok("CommandLine send OK plant");
+				}
+				else 
+				{
+					return ResponseEntity.status(490).body("ERROR 490");
+				}
+			}
+			else
+			{
+				return ResponseEntity.status(404).body("ERROR 404");
+			}
+		}
+		catch (Exception exception)
+		{
+			return ResponseEntity.status(500).body("ERROR 500");
+		}
+
+	}
+
+}
