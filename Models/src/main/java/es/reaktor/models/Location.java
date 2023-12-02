dce8dc116d44bcc399b6316af425870394935657
Juan Sutil <jsutmes2712@g.educaand.es>
2023-12-02 11:21:09 +0100

    creacion clases Location, MonitorizationLog, Status

---
+++ Models/src/main/java/es/reaktor/models/Location.java
@@ -0,0 +1,18 @@
+package es.reaktor.models;
+
+import lombok.Data;
+
+/**
+ * @author Juan Sutil Mesa
+ */
+@Data
+public class Location
+{
+	
+	private String classroom;
+	
+	private int plant;
+	
+	private String trolley;
+	
+}
