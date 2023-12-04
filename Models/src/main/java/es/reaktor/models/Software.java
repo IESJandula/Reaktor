514c6c51df3344148d24f8e37d4324cfc08c250c
Carlos <cruiara0501@g.educaand.es>
2023-12-04 20:21:32 +0100

    REAK_OBJ_201

---
+++ Models/src/main/java/es/reaktor/models/Software.java
@@ -0,0 +1,43 @@
+package es.reaktor.models;
+
+public class Software 
+{
+
+		private String aplication;
+		
+		
+		/**
+		 * 
+		 */
+		public Software() {
+			super();
+		}
+
+		/**
+		 * 
+		 * @param aplication
+		 */
+		public Software(String aplication) {
+			super();
+			this.aplication = aplication;
+		}
+		
+		/**
+		 * 
+		 * @return aplication
+		 */
+		public String getAplication() {
+			return aplication;
+		}
+		
+		/**
+		 * 
+		 * @param aplication
+		 */
+		public void setAplication(String aplication) {
+			this.aplication = aplication;
+		}
+		
+		
+		
+}
