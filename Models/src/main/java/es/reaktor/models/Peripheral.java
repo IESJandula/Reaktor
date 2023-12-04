d5a35f9ce52c634d6d0c376de217b4e563cd9542
Juan Sutil <jsutmes2712@g.educaand.es>
2023-12-04 20:35:35 +0100

    error peripheral

--- Models/src/main/java/es/reaktor/models/Peripheral.java
+++ Models/src/main/java/es/reaktor/models/Peripheral.java
@@ -1,28 +1,13 @@
 package es.reaktor.models;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class Peripheral extends HardwareComponent
 {
 	/** Attribute is open*/
 	private Boolean isOpen;
 
-	public Peripheral (String component, int cuantity, boolean isOpen) {
-		super(component, cuantity);
-		this.isOpen = isOpen;
-	}
-
-	/**
-	 * @return the isOpen
-	 */
-	public Boolean getIsOpen()
-	{
-		return isOpen;
-	}
-
-	/**
-	 * @param isOpen the isOpen to set
-	 */
-	public void setIsOpen(Boolean isOpen)
-	{
-		this.isOpen = isOpen;
-	}
 }
