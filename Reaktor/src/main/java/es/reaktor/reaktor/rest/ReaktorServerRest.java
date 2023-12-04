d351c8fbd5c8056059303989a268eb72617f1e6c
Juan Sutil <jsutmes2712@g.educaand.es>
2023-12-04 12:16:25 +0100

    _

--- Reaktor/src/main/java/es/reaktor/reaktor/rest/ReaktorServerRest.java
+++ Reaktor/src/main/java/es/reaktor/reaktor/rest/ReaktorServerRest.java
@@ -176,7 +176,7 @@ public class ReaktorServerRest
     		
     		return ResponseEntity.ok().build();
     	}catch (ComputerError computerError){
-    		return ResponseEntity.status(400).body(computerError));
+    		return ResponseEntity.status(400).body(computerError);
     	}catch (Exception e) {
     		return ResponseEntity.status(500).body(e.getMessage());
 		}
