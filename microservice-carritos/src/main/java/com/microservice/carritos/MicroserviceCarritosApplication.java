9788e93cc60bd742dccf1544d68615723b15b776
jesus guerrero <jesusguerrerogonzalez2@gmail.com>
2023-12-05 11:16:00 +0100

    Added carritos microservice, its create the database carritos, but is empty.

---
+++ microservice-carritos/src/main/java/com/microservice/carritos/MicroserviceCarritosApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.carritos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceCarritosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceCarritosApplication.class, args);
+	}
+
+}
---
+++ microservice-carritos/src/test/java/com/microservice/carritos/MicroserviceCarritosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.carritos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceCarritosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
