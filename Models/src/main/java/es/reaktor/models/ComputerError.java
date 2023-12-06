f571cc56ed624bd842224a2ea19cc4ca7e299ba3
manumartin0709 <manuelmartinmurillo@gmail.com>
2023-12-06 16:47:25 +0100

    creacion del endpoint 406

---
+++ Models/src/main/java/es/reaktor/models/ComputerError.java
@@ -0,0 +1,39 @@
+package es.reaktor.models;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ComputerError extends Exception
+{
+	/**
+	 * Serial Version UID
+	 */
+	private static final long serialVersionUID = 2430243519965740823L ;
+	
+	/** Attribute - Code */
+	private int code ;
+	
+	/** Attribute - Message */
+	private String message ;
+
+	/**
+	 * @param code	  with the code
+	 * @param message with the message
+	 */
+	public ComputerError(int code, String message)
+	{
+		this.code    = code ;
+		this.message = message ;
+	}
+
+	public Object getBodyExceptionMessage()
+	{
+		Map<String, Object> mapBodyException = new HashMap<>() ;
+		
+		mapBodyException.put("code", this.code) ;
+		mapBodyException.put("message", this.message) ;
+		
+		return mapBodyException ;
+	}
+
+}
--- Reaktor/src/main/java/es/reaktor/reaktor/rest/ReaktorServerRest.java
+++ Reaktor/src/main/java/es/reaktor/reaktor/rest/ReaktorServerRest.java
@@ -1,23 +1,31 @@
 package es.reaktor.reaktor.rest;
 
-import es.reaktor.models.DTO.MalwareDTOWeb;
-import es.reaktor.models.DTO.ReaktorDTO;
-import es.reaktor.models.DTO.SimpleComputerDTO;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import es.reaktor.models.ComputerError;
 import es.reaktor.models.Malware;
 import es.reaktor.models.Motherboard;
 import es.reaktor.models.Reaktor;
+import es.reaktor.models.DTO.MalwareDTOWeb;
+import es.reaktor.models.DTO.ReaktorDTO;
+import es.reaktor.models.DTO.SimpleComputerDTO;
 import es.reaktor.reaktor.reaktor_actions.ReaktorActions;
 import es.reaktor.reaktor.reaktor_actions.ReaktorService;
 import es.reaktor.reaktor.repository.IMalwareRepository;
 import es.reaktor.reaktor.repository.IMotherboardRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Date;
-import java.util.List;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -162,5 +170,37 @@ public class ReaktorServerRest
     public ResponseEntity<ReaktorDTO> getReaktor(@PathVariable String idComputer )
     {
         return ResponseEntity.ok(reaktorService.getInformationReaktor(idComputer));
+    }
+    
+    @RequestMapping(method = RequestMethod.GET, value = "/computer/admin/screenshot", produces ="application/zip")
+    public ResponseEntity<?> sendScreenshotOrder(
+    		 @RequestHeader (required = false) String classroom,
+    		 @RequestHeader (required = false) String trolley
+    		 )
+    {
+    	try 
+    	{
+    		controlScreenShot(classroom, trolley);
+    		return ResponseEntity.ok().build();
+    	}
+    	catch (ComputerError computerError) 
+    	{
+    		return ResponseEntity.status(400).body(computerError.getBodyExceptionMessage()) ;
+		}
+		catch (Exception e )
+		{
+			
+			return ResponseEntity.status(500).body(e.getMessage()) ;
+		} 
+    }
+    
+    
+    public void controlScreenShot(String classroom, String trolley) throws ComputerError
+    {
+    	if(classroom.isEmpty() && trolley.isEmpty()) 
+    	{
+    		
+    		throw new ComputerError(1, "Nose ha enviado ninguna clase o carrito");
+    	}
     }
 }
