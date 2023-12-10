7832b61086d10c140ac4c4a40c9c5873dccb4f30
Carlos <cruiara0501@g.educaand.es>
2023-12-10 21:08:10 +0100

    Añadir @CrossOrigin y @Slf4j

--- Horarios/src/main/java/es/reaktor/horarios/rest/HorariosRest.java
+++ Horarios/src/main/java/es/reaktor/horarios/rest/HorariosRest.java
@@ -1,9 +1,15 @@
 package es.reaktor.horarios.rest;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -11,11 +17,14 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
+@RequestMapping("/horarios")
+@Slf4j
 public class HorariosRest {
 
-    @PostMapping("/procesar-archivo")
-    public List<String> procesarArchivoCSV(@RequestParam("archivo.csv") MultipartFile archivo) {
+    @RequestMapping(method = RequestMethod.POST, value = "/send/csv", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public List<String> sendCsvTo(@RequestPart MultipartFile archivo) {
         List<String> roles = new ArrayList<>();
 
         try (BufferedReader br = new BufferedReader(new InputStreamReader(archivo.getInputStream()))) {
