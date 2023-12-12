cbb1c6c0f092053aa4d863d79aa92e2a98993512
PabloRuiz1312 <pruican1312@g.educaand.es>
2023-12-12 19:54:43 +0100

    Se crea el modelo para guardar datos del csv

---
+++ Horarios/src/main/java/es/iesjandula/horarios/models/csv/ModelCSV.java
@@ -0,0 +1,28 @@
+package es.iesjandula.horarios.models.csv;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 
+ * @author Pablo Ruiz Canovas
+ *
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ModelCSV 
+{
+	/**Nombre de la persona del csv */
+	private String nombre;
+	/**Apellido de la persona del csv */
+	private String apellido;
+	/**Cuenta de correo de la persona del csv */
+	private String email;
+	/**Lista de roles de la persona del csv */
+	private String [] roles;
+}
