949b6fb10b317bf07790e2bacb66fff621f121f6
PabloRuiz1312 <pruican1312@g.educaand.es>
2023-12-12 20:47:40 +0100

    Merge del endpoint 4

--- Horarios/src/main/java/es/iesjandula/horarios/rest/RestHandlerHorarios.java
+++ Horarios/src/main/java/es/iesjandula/horarios/rest/RestHandlerHorarios.java
@@ -7,6 +7,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import org.springframework.web.multipart.MultipartFile;
 import es.iesjandula.horarios.exception.HorarioError;
 import es.iesjandula.horarios.models.csv.ModelCSV;
 import es.iesjandula.horarios.utils.ICSVParser;
+import es.iesjandula.horarios.utils.IChecker;
 
 /**
  * 
@@ -23,7 +25,7 @@ import es.iesjandula.horarios.utils.ICSVParser;
  */
 @RestController
 @RequestMapping( value = "/horarios",produces = "application/json")
-public class RestHandlerHorarios implements ICSVParser
+public class RestHandlerHorarios implements ICSVParser,IChecker
 {
 	/**Logger de la clase */
 	private static Logger log = LogManager.getLogger();
@@ -62,6 +64,33 @@ public class RestHandlerHorarios implements ICSVParser
 		{
 			log.error("Error interno de servidor",ex);
 			return ResponseEntity.status(500).body("Error interno de servidor");
+		}
+	}
+	/**
+	 * Endpoint que recibe un email y busca a partir de el la lista de roles de la cuenta asociada
+	 * @param email de la cuenta a buscar 
+	 * @return ok si encontro los roles, 404 si el email no es valido o 500 si hubo un error interno
+	 */
+	@RequestMapping(method = RequestMethod.GET,value = "/get/roles")
+	public ResponseEntity<?> getRoles(@RequestHeader(value = "email",required = true) final String email)
+	{
+		try
+		{
+			//Comprobacion del email y si existe en los modelos
+			this.checkParam(email, this.modelos);
+			//Obtencion de los roles
+			String [] roles = this.searchRol(email, modelos);
+			return ResponseEntity.ok().body(roles);
+		}
+		catch(HorarioError ex)
+		{
+			log.error("Error en el email o al buscar el modelo",ex);
+			return ResponseEntity.status(404).body(ex.getBodyMessageException());
+		}
+		catch(Exception ex)
+		{
+			log.error("Error interno de servidor",ex);
+			return ResponseEntity.status(404).body("Error de servidor");
 		}
 	}
 		
