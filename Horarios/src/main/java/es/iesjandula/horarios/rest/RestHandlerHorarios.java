757f3e37ab098bd2501f1570c79f7146c0181336
Javier Martinez <jmarmeg1005@g.educaand.es>
2023-12-14 09:15:07 +0100

    nuevo endpoint

--- Horarios/src/main/java/es/iesjandula/horarios/rest/RestHandlerHorarios.java
+++ Horarios/src/main/java/es/iesjandula/horarios/rest/RestHandlerHorarios.java
@@ -1,8 +1,12 @@
 package es.iesjandula.horarios.rest;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -10,6 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import es.iesjandula.horarios.exception.HorarioError;
+import es.iesjandula.horarios.models.xml.Aula;
+import es.iesjandula.horarios.models.xml.Centro;
+import es.iesjandula.horarios.models.xml.Profesor;
+import es.iesjandula.horarios.models.xml.Tramo;
 import es.iesjandula.horarios.utils.ParserXml;
 
 /**
@@ -23,7 +31,7 @@ public class RestHandlerHorarios implements ParserXml
 {
 	/**Logger de la clase */
 	private static Logger log = LogManager.getLogger();
-
+	private static Centro centro= null;
 	/**
 	 * Constructor por defecto
 	 */
@@ -32,6 +40,11 @@ public class RestHandlerHorarios implements ParserXml
 		
 	}
 	
+	/**
+	 * 
+	 * @param xml
+	 * @return
+	 */
 	@RequestMapping(method = RequestMethod.POST, value = "/send/xml", consumes = "multipart/form-data")
     public ResponseEntity<?> parseXML(
             @RequestPart MultipartFile xml)
@@ -40,7 +53,8 @@ public class RestHandlerHorarios implements ParserXml
 		{
 			if(chekIfEmptyAndXml(xml))
 			{
-				return ResponseEntity.ok(this.parserFileToObject(xml).getDatos());
+				centro = this.parserFileToObject(xml);
+				return ResponseEntity.ok(centro.getDatos());
 	    	}
 			else
 			{
@@ -74,5 +88,73 @@ public class RestHandlerHorarios implements ParserXml
 			
 		}
 		return false;
+	}
+	
+	/**
+	 * 
+	 * @param xml
+	 * @return
+	 */
+	@RequestMapping(method = RequestMethod.POST, value = "/teacher/get/classroom", produces = "multipart/form-data")
+    public ResponseEntity<?> getClassroomTeacher(
+            @RequestHeader String profesorName,
+            @RequestHeader String profesorSurname)
+    {
+		try
+		{
+			
+				return ResponseEntity.ok("");
+	    	
+		}
+		catch (HorarioError exception)
+		{
+			String error = "Is not a xml or is empty";
+			HorarioError horarioError = new HorarioError(404, error);
+			return ResponseEntity.status(404).body(horarioError.getBodyMessageException());
+		}
+        catch (Exception exception)
+		{     	
+			log.error(exception.getMessage());
+			HorarioError horarioError = new HorarioError(500, exception.getMessage());
+			return ResponseEntity.status(500).body(horarioError.getBodyMessageException());
+		}
+    }
+	
+	
+	private Aula getAulaByProfesorName(String name, String surneme)
+	{	
+		String horaActual = getActualHour();
+		List<Profesor> listaProfesores = centro.getDatos().getProfesor();
+		List<Tramo> listaTramos = centro.getDatos().getTramo();
+		int numeroProfesor = 0;
+		int numeroTramo = 0;
+		
+		for(Profesor x : listaProfesores)
+		{
+			if(x.getNombre().equalsIgnoreCase(surneme+", "+name))
+			{
+				numeroProfesor=x.getNum_int_pr();
+			}
+		}
+		for(Tramo x : listaTramos)
+		{
+			x.getHora_final();
+			x.getHora_inicio();
+			if(x.getNombre().equalsIgnoreCase(surneme+", "+name))
+			{
+				numeroProfesor=x.getNum_int_pr();
+			}
+		}
+		return null;	
+	}
+
+	private String getActualHour()
+	{
+		
+		LocalDateTime locaDate = LocalDateTime.now();
+		int hours  = locaDate.getHour();
+		int minutes = locaDate.getMinute();
+		
+		return  hours  +":"+ minutes ;
 	}
 }
