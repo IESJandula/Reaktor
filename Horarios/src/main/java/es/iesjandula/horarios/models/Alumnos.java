81765dcefd7f3203e2e28236e207458b2f66f182
PabloRuiz1312 <pruican1312@g.educaand.es>
2023-12-15 10:49:20 +0100

    Creacion del modelo alumnos

---
+++ Horarios/src/main/java/es/iesjandula/horarios/models/Alumnos.java
@@ -0,0 +1,19 @@
+package es.iesjandula.horarios.models;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+/**
+ * @author Pablo Ruiz Canovas
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Alumnos 
+{
+	/**Nombre del estudiante */
+	private String nombre;
+	/**Apellido del estudiante */
+	private String apellido;
+	/**Curso en el que se encuentra el alumno */
+	private String curso;
+}
