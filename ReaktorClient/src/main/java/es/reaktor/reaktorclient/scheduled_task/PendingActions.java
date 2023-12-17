ae38371a7468bbf64422e5abca4d79eecf411066
Carlos <cruiara0501@g.educaand.es>
2023-12-17 20:26:07 +0100

    Reset method add to client

--- ReaktorClient/src/main/java/es/reaktor/reaktorclient/scheduled_task/PendingActions.java
+++ ReaktorClient/src/main/java/es/reaktor/reaktorclient/scheduled_task/PendingActions.java
@@ -1,14 +1,11 @@
 package es.reaktor.reaktorclient.scheduled_task;
 
 import es.reaktor.models.Action;
-import es.reaktor.models.ComputerError;
 import es.reaktor.reaktorclient.utils.exceptions.ConstantsErrors;
 import lombok.extern.slf4j.Slf4j;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
-
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -18,147 +15,102 @@ import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Component
 @Slf4j
-public class PendingActions
-{
-
+public class PendingActions {
 	@Value("${reaktor.server.url}")
 	private String reaktorServerUrl;
 
 	@Scheduled(fixedDelayString = "${reaktor.pendingActions}", initialDelay = 2000)
-	public void computerOnReport()
-	{
+	public void computerOnReport() {
 		CloseableHttpClient httpClient = null;
 		CloseableHttpResponse response = null;
-		try
-		{
+		try {
 			httpClient = HttpClients.createDefault();
-
 			String url = reaktorServerUrl + "/computers/get/status";
-
 			HttpGet request = new HttpGet(url);
-
-			request.addHeader("serialNumber", "002");
-
+			request.addHeader("serialNumber", "003");
 			response = httpClient.execute(request);
 			Action action = new ObjectMapper().readValue(EntityUtils.toString(response.getEntity()), Action.class);
-			if (action.getActionName() != null)
-			{
-				switch (action.getActionName())
-				{
-				case "uninstall" -> uninstallApp(action.getInfo());
-				case "openWeb" -> openWeb(action.getInfo());
+			if (action.getActionName() != null) {
+				switch (action.getActionName()) {
+				case "reset" -> reset();
 				default -> System.out.println("Esto esta sin implementar makina");
 				}
 			}
-
-		} catch (ClientProtocolException e)
-		{
+		} catch (ClientProtocolException e) {
 			log.warn(e.getMessage());
+			log.warn(ConstantsErrors.ERROR_COMMUNICATION_TO_SERVER, e);
 			e.printStackTrace();
-		} catch (IOException e)
-		{
+		} catch (IOException e) {
 			log.warn(e.getMessage());
+			log.warn(ConstantsErrors.ERROR_COMMUNICATION_TO_SERVER, e);
 			e.printStackTrace();
-		} catch (ComputerError e)
-		{
-			log.warn(e.getMessage());
-			e.printStackTrace();
-		} finally
-		{
+		} finally {
 			this.close(httpClient, response);
 		}
 	}
-	
-	private void close(CloseableHttpClient httpClient, CloseableHttpResponse response)
-	{
-		try
-		{
-			response.close();
-		} catch (IOException e)
-		{
-			log.warn(e.getMessage());
-			log.warn(ConstantsErrors.ERROR_COMMUNICATION_TO_SERVER, e);
-			e.printStackTrace();
-		}
-		try
-		{
-			httpClient.close();
-		} catch (IOException e)
-		{
-			log.warn(e.getMessage());
-			log.warn(ConstantsErrors.ERROR_COMMUNICATION_TO_SERVER, e);
-			e.printStackTrace();
-		}
-	}
-	
-	private void openWeb(String url) throws ComputerError {
-		
-		try
-		{
-			Process proceso = new ProcessBuilder("cmd.exe", "/c", "start " + url).start();
-			
-			proceso.waitFor();
-			
-		} catch (IOException e)
-		{
-			log.warn(e.getMessage());
-			e.printStackTrace();
-			throw new ComputerError(1, url, e);
-		} catch (InterruptedException e)
-		{
-			log.warn(e.getMessage());
-			e.printStackTrace();
-			throw new ComputerError(2, url, e);
-		}
-		
-	}
-	
-	private void uninstallApp(String appName)
-	{
 
+	private void uninstallApp(String appName) {
 		Scanner scanner = null;
-		try
-		{
+		try {
 			log.info("inicio busqueda chrome");
 			Process proceso = new ProcessBuilder("cmd.exe", "/c", "dir /s /b C:\\" + appName + " > resultados.txt")
 					.start();
-			
 			int resultado = proceso.waitFor();
 			log.info("fin busqueda chrome");
-			if (resultado == 0)
-			{
-				
+			if (resultado == 0) {
 				scanner = new Scanner(new File("resultados.txt"));
-
 				String path = scanner.nextLine();
 				log.info("inicio borrado resultado");
-				proceso = new ProcessBuilder("cmd.exe", "/c", "del resultados.txt")
-						.start();
+				proceso = new ProcessBuilder("cmd.exe", "/c", "del resultados.txt").start();
 				resultado = proceso.waitFor();
 				log.info("inicio desinstalacion");
-				proceso = new ProcessBuilder("cmd.exe", "/c", "\"" + path + "\" --uninstall --force-uninstall")
-						.start();
-
+				proceso = new ProcessBuilder("cmd.exe", "/c", "\"" + path + "\" --uninstall --force-uninstall").start();
 				resultado = proceso.waitFor();
 				log.info("fin desinstalacion");
-			} else
-			{
+			} else {
 				System.out.println("Error en la desinstalación. Código de salida: " + resultado);
 			}
-		} catch (IOException | InterruptedException e)
-		{
+		} catch (IOException | InterruptedException e) {
 			e.printStackTrace();
-		} finally
-		{
-			if (scanner != null)
-			{
+		} finally {
+			if (scanner != null) {
 				scanner.close();
 			}
+		}
+	}
+	private void reset() {
+		Scanner scanner = null;
+		try {
+			Process proceso = new ProcessBuilder("cmd.exe", "/c", "shutdown /r /t 60").start();
+			int resultado = proceso.waitFor();
+			if (resultado == 0) {
+				log.info("El ordenador se reiniciara en 60 segundos");
+			} else {
+				System.out.println("Error al reiniciar. Código de salida: " + resultado);
+			}
+		} catch (IOException | InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void close(CloseableHttpClient httpClient, CloseableHttpResponse response) {
+		try {
+			response.close();
+		} catch (IOException e) {
+			log.warn(e.getMessage());
+			log.warn(ConstantsErrors.ERROR_COMMUNICATION_TO_SERVER, e);
+			e.printStackTrace();
+		}
+		try {
+			httpClient.close();
+		} catch (IOException e) {
+			log.warn(e.getMessage());
+			log.warn(ConstantsErrors.ERROR_COMMUNICATION_TO_SERVER, e);
+			e.printStackTrace();
 		}
 	}
 }
