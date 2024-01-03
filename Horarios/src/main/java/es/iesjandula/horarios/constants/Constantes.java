1cbf9444c20baa19db5c8d3be836542f0cb82e1f
PabloRuiz1312 <pruican1312@g.educaand.es>
2024-01-03 14:58:40 +0100

    Se elimina el cargado de alumnos

--- Horarios/src/main/java/es/iesjandula/horarios/constants/Constantes.java
+++ Horarios/src/main/java/es/iesjandula/horarios/constants/Constantes.java
@@ -10,21 +10,6 @@ import es.iesjandula.horarios.models.Alumno;
  */
 public final class Constantes 
 {
-	/**CARGADO DE ALUMNOS*/
-	public static final Alumno [] cargarAlumnos()
-	{
-		Alumno [] alumnos = new Alumno [8];
-		alumnos [0] = new Alumno("Pablo", "Ruiz Canovas","2DAM");
-		alumnos [1] = new Alumno("Javier", "Martinez Megias","2DAM");
-		alumnos [2] = new Alumno("Eduardo", "Moreno Algaba","2DAM");
-		alumnos [3] = new Alumno("Oliver", "Rufian Baudet","1BACH");
-		alumnos [4] = new Alumno("Alberto", "Soto Pascual","2BACH");
-		alumnos [5] = new Alumno("Lucia", "Diaz Gutierrez","4ESO");
-		alumnos [6] = new Alumno("Fernando", "Torres Arias","3ESO");
-		alumnos [7] = new Alumno("Maria", "Gomez Perez","1ESO");
-		return alumnos;
-	}
-	
 	/**CARGADO DE HORAS */
 	public static final Map<String,List<String>> cargarHoras()
 	{
