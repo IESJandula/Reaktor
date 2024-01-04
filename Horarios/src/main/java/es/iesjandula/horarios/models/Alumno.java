cf831027687177eacd9886bb381f8bf56837138b
PabloRuiz1312 <pruican1312@g.educaand.es>
2024-01-04 13:50:15 +0100

    Se adapta la clase alumnos para los endpoints 19 y 20

--- Horarios/src/main/java/es/iesjandula/horarios/models/Alumno.java
+++ Horarios/src/main/java/es/iesjandula/horarios/models/Alumno.java
@@ -1,12 +1,10 @@
 package es.iesjandula.horarios.models;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 /**
  * @author Pablo Ruiz Canovas
  */
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public class Alumno implements Comparable<Alumno>
 {
@@ -18,6 +16,24 @@ public class Alumno implements Comparable<Alumno>
 	private String dni;
 	/**Curso en el que se encuentra el alumno */
 	private String curso;
+	/**Numero de veces que el alumno ha ido al baño */
+	private int numBaño;
+	
+	/**
+	 * Constructor completo que crea un alumno e inicializa las veces que ha ido al baño a 0
+	 * @param apellido
+	 * @param nombre
+	 * @param dni
+	 * @param curso
+	 */
+	public Alumno(String apellido, String nombre, String dni, String curso) {
+		super();
+		this.apellido = apellido;
+		this.nombre = nombre;
+		this.dni = dni;
+		this.curso = curso;
+		this.numBaño = 0;
+	}
 	
 	@Override
 	public int compareTo(Alumno otro) 
@@ -25,4 +41,5 @@ public class Alumno implements Comparable<Alumno>
 		//Se compara por los apellidos del alumno
 		return this.apellido.compareTo(otro.apellido);
 	}
+
 }
