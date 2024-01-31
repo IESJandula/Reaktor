6b3976aa1e0fef73f4522b450400f956733bb1bf
DavidMartinezFlores <dmarflo526@g.educaand.es>
2024-01-31 08:17:04 +0100

    Rename Classroom cfg

---
+++ MonitoringWebNode/src/app/principal/principal.component.ts
@@ -0,0 +1,29 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {SimpleComputerDTO} from '../computer-dto';
+import {interval, Subscription} from "rxjs";
+import {URL_SERVER} from "../constants";
+
+@Component({
+  selector: 'principal-compo',
+  templateUrl: './principal.component.html',
+  styleUrls: ['./principal.component.css']
+})
+export class PrincipalComponent implements OnInit {
+  computers?: SimpleComputerDTO[];
+  subscription: Subscription | undefined;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getComputers();
+    this.subscription = interval(1000).subscribe(x => { this.getComputers(); });
+
+  }
+
+  private getComputers() {
+    this.http.get<SimpleComputerDTO[]>(URL_SERVER + '/computer').subscribe(data => {
+      this.computers = data;
+    });
+  }
+}
